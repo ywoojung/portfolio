be69fcadabbeda0c4455eac022039abbb95cc2a8
ywoojung <y_wj1025@yahoo.co.kr>
2021-01-26 14:54:32 +0900

    index

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>윤우정의 포트폴리오</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Noto+Sans+KR:100,300,400,500,700,900&display=swap&subset=korean" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Monoton|Noto+Sans+KR:100,300,400,500,700,900|Raleway:100,200,300,400,500,600,700,800,900&display=swap&subset=korean" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
+  </head>
+  <body>
+    <div id="wrap" style="font-family: 'Montserrat';">
+      <div class="logo">
+        <a href="index.php" class="pc_logo">
+          <h1>
+            우 정<span>WOOJUNG<br>YUN 931025</span>
+          </h1>
+        </a>
+        <a href="index.php" class="mobile_logo">
+          <h1>
+            <span>WOOJUNG YUN<br>931025</span>
+          </h1>
+        </a>
+      </div><!-- logo -->
+
+      <div class="about clear">
+        <div class="content_tit">
+          <h3>ABOUT</h3>
+        </div>
+        <div class="left">
+          <img src="img/face.png" alt="">
+        </div>
+        <div class="right">
+          <p>윤우정 YUN WOOJUNG</p>
+          <p>1993. 10. 25</p>
+          <p>서울시 송파구</p>
+          <p>010 4111 3983</p>
+          <p>y_wj1025@yahoo.co.kr</p>
+          <p>KaKaotalk ID : wooj1025</p>
+        </div>
+      </div><!-- about -->
+
+      <div class="info">
+        <div class="info_content">
+          <p class="line">
+            <span></span>
+          </p>
+          <ul class="info_content_box">
+            <li class="skills">
+              <div class="content_tit">
+                <h3>SKILLS</h3>
+              </div>
+              <div class="info_content_txt clear">
+                <div class="left clear">
+                  <div class="circle-big circle04">
+              			<div class="text">
+              				Jquery
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+                  <div class="circle-big circle03">
+              			<div class="text">
+              				JavaScript
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+                  <div class="circle-big circle02">
+              			<div class="text">
+              				CSS
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+
+                  <div class="circle-big circle01">
+              			<div class="text">
+              				HTML
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+                </div>
+                <div class="right clear">
+                  <div class="circle-big circle05">
+              			<div class="text">
+              				Photoshop
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+                  <div class="circle-big circle06">
+              			<div class="text">
+              				Illstrator
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+                  <div class="circle-big circle07">
+              			<div class="text">
+              				PHP
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+                  <div class="circle-big circle08">
+              			<div class="text">
+              		    SQL
+              			</div>
+              			<svg>
+              				<circle class="bg" cx="57" cy="57" r="52"></circle>
+              				<circle class="progress" cx="57" cy="57" r="52"></circle>
+              			</svg>
+              		</div>
+                </div>
+              </div>
+            </li>
+            <li class="portfolio">
+              <div class="content_tit">
+                <h3>PORTFOLIO</h3>
+              </div>
+              <div class="info_content_txt lotte clear" data-aos="fade-right" data-aos-duration="2000">
+                <div class="left">
+                  <p>
+                    <span class="txt_01">롯데칠성음료 벤치마킹 사이트</span>
+                    <span class="txt_02">Adaptive Web Design</span>
+                    <a href="http://yunwoojung.dothome.co.kr/sub/sub_lottechilsung.html">View more</a>
+                  </p>
+                </div>
+                <div class="right"></div>
+              </div>
+              <div class="info_content_txt php clear" data-aos="fade-left" data-aos-duration="2000">
+                <div class="left"></div>
+                <div class="right">
+                  <p>
+                    <span class="txt_01">PHP & MySQL 웹 프로그래밍 및 사이트 디자인, 리뉴얼</span>
+                    <span class="txt_02">Responsive Web Design</span>
+                    <a href="http://yunwoojung.dothome.co.kr/sub/sub_php.html">View more</a>
+                  </p>
+                </div>
+              </div>
+              <div class="info_content_txt mammoth clear" data-aos="fade-right" data-aos-duration="2000">
+                <div class="left">
+                  <p>
+                    <span class="txt_01">매머드커피 벤치마킹 사이트</span>
+                    <span class="txt_02">Responsive Web Design</span>
+                    <a href="http://yunwoojung.dothome.co.kr/sub/sub_mammoth.html">View more</a>
+                  </p>
+                </div>
+                <div class="right"></div>
+              </div>
+              <div class="info_content_txt doosan clear" data-aos="fade-left" data-aos-duration="2000">
+                <div class="left"></div>
+                <div class="right">
+                  <p>
+                    <span class="txt_01">두산중공업 리뉴얼 사이트</span>
+                    <span class="txt_02">Responsive Web Design</span>
+                    <a href="http://yunwoojung.dothome.co.kr/sub/sub_doosan.html">View More</a>
+                  </p>
+                </div>
+              </div>
+              <div class="info_content_txt fiore clear" data-aos="fade-right" data-aos-duration="2000">
+                <div class="left">
+                  <p>
+                    <span class="txt_01">기획/디자인/개발 사이트</span>
+                    <span class="txt_02">Responsive Web Design</span>
+                    <a href="http://yunwoojung.dothome.co.kr/sub/sub_fiore.html">View More</a>
+                  </p>
+                </div>
+                <div class="right"></div>
+              </div>
+            </li>
+          </ul>
+        </div><!-- info_content -->
+      </div><!-- info -->
+
+      <div class="contact">
+        <div class="content_tit">
+          <h3>
+            <a href="php/message_check.php" style="cursor: default;">CONTACT</a>
+          </h3>
+        </div>
+        <div class="contact_form">
+          <form name="message_form" action="php/send_db.php" method="post">
+            <input type="text" name="name" placeholder="이름 또는 회사명" class="name">
+            <input type="text" name="email" placeholder="이메일" class="email">
+            <textarea name="message" placeholder="메세지를 남겨주세요" class="message"></textarea>
+            <input type="button" name="" value="전송하기" class="send" onclick="send_message()">
+          </form>
+        </div>
+      </div>
+
+        <div class="top_btn">
+        <p>
+          <a href="#">TOP</a>
+        </p>
+      </div>
+
+
+      <footer>
+        <p>Copyright &copy; 2020 All rights reserved by WooJung Yun</p>
+      </footer>
+
+    </div><!-- wrap -->
+  </body>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="js/aos.js"></script>
+  <script type="text/javascript" src="js/custom.js"></script>
+  <script type="text/javascript">
+    function send_message() {
+      if (!document.message_form.name.value) {
+        alert('이름 또는 회사명을 입력해주세요.');
+        document.message_form.name.focus();
+        return;
+      }
+      if (!document.message_form.email.value) {
+        alert('이메일을 입력해주세요.');
+        document.message_form.email.focus();
+        return;
+      }
+      if (!document.message_form.message.value) {
+        alert('메세지를 입력해주세요.');
+        document.message_form.message.focus();
+        return;
+      }
+      document.message_form.submit();
+    }
+  </script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $(window).scroll(function() {
+
+        var scroll=$(window).scrollTop();
+        var infoTop=$(".info_content").offset().top;
+
+        if (scroll>=infoTop) {
+          $(".top_btn").show();
+        } else {
+          $(".top_btn").hide();
+        }
+
+      })
+    });
+  </script>
+</html>
